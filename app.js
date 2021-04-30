5c65b139ff97528ad257ae827beb7068eb3b71ec
Dean Tan <deantan@lync2m.com>
2021-04-30 14:18:29 +0800

    feat:提交

---
+++ app.js
@@ -0,0 +1,125 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+/* particlesJS.load("app", "./particles.json", function () {
+  console.log("callback - particles.js config loaded");
+}); */
+
+window.onload = function () {
+  initParticles("app1", config);
+  initParticles("app2", config);
+  initParticles("app3", config);
+ 
+  function initParticles(id) {
+    particlesJS(id, config);
+  }
+};
+
+const config = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 200,
+      },
+    },
+    color: {
+      value: "#bfbbae",
+    },
+    shape: {
+      type: "image",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/dogecoin-300.png",
+        width: 200,
+        height: 200,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 10,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#bfbbae",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab",
+      },
+      onclick: {
+        enable: false,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 140,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 200,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+};
